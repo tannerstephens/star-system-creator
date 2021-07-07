5f4da04ee2d2c8710bc59628c714ca4ccf96284d
Tanner Stephens <tannerrstephens@gmail.com>
2021-07-07 00:36:45 -0500

    empty pages

--- projectSettings.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  title: 'yarnTemplate',
-  repo: ''
-};
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import App from './app';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const rootElement = document.getElementById('root');
-
-  const app = new App(window);
-  app.display(rootElement, document);
-})
