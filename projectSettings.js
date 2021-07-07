a370352e6bb5d37d21f2b68e755ade51f343092d
Tanner Stephens <tannerrstephens@gmail.com>
2021-07-07 17:04:20 -0500

    Adds license to bottom of page

--- projectSettings.js
+++ projectSettings.js
@@ -1,4 +1,5 @@
 module.exports = {
   title: 'Star System Creator',
-  repo: 'https://github.com/tannerstephens/star-system-creator'
+  repo: 'https://github.com/tannerstephens/star-system-creator',
+  license: 'https://github.com/tannerstephens/star-system-creator/blob/master/LICENSE.md'
 };
